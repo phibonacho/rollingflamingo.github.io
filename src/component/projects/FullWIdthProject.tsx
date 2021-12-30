00076a9c9f18acaf6f218c20081c6e441af99aee
Andrea Storace <andrea.storace.uni@gmail.com>
2021-12-30 23:13:33 +0100

    added scroll animation to full width project.

--- src/component/projects/FullWIdthProject.tsx
+++ src/component/projects/FullWIdthProject.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, {useEffect, useRef} from 'react';
+import {gsap} from 'gsap';
 import "./fwProject.scss";
 
 import ProjectDecorationUp from "../graphics/ProjectDecorationUp";
@@ -15,7 +15,38 @@ export interface IFullWidthProject {
     theme?: 'blue' | 'pink' | 'purple' | 'teal' | 'yellow';
 }
 
+const rangedRandom = (min: number, max: number) => Math.random() * (max - min) + min;
+
 export default function FullWidthProject(props: IFullWidthProject) {
+    const containerRef = useRef(null);
+    useEffect(() => {
+        const current = containerRef.current;
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: current,
+                start: 'top bottom',
+                end: 'bottom center',
+                scrub: .1
+            }
+        });
+
+        // @ts-ignore
+        const selector = current.querySelector('.project-image');
+        // @ts-ignore
+        const d1 = current.querySelector('.project-ud');
+        // @ts-ignore
+        const d2 = current.querySelector('.project-dd');
+
+        // add animations and labels to the timeline
+
+        tl
+            .from(selector, { rotation: rangedRandom(7, 15), x: rangedRandom(30, 60), y: rangedRandom(30, 60), scale: 1.4 }, 0)
+            .from(d1, {x: -rangedRandom(60, 80), y: rangedRandom(30, 50), rotation: rangedRandom(8, 16), duration: 1}, 0)
+            .from(d2, {x: -rangedRandom(60, 80), y: -rangedRandom(120, 160), rotation: rangedRandom(8, 16), duration: 1}, 0);
+
+    });
+
     let colors = {
         background: props.theme || 'white',
         text: props.theme ? `${props.theme}-darkest` : 'obsidian',
@@ -23,20 +54,20 @@ export default function FullWidthProject(props: IFullWidthProject) {
         lowerDecoration: props.theme? `${props.theme}-dark` : 'obsidian'
     };
 
-    return <article className={`full-width-project bg-${colors.background}`} id={`${props.title}-section`} aria-labelledby={`${props.title}-title`}>
+    return <article ref={containerRef} className={`full-width-project bg-${colors.background}`} id={`${props.title}-section`} aria-labelledby={`${props.title}-title`}>
         <div className="min-h-screen xl:max-w-screen-xxl lg:max-w-screen-md mx-auto px-2 py-20 md:p-20  flex flex-row flex-wrap items-center">
             <div className="xl:order-2 flex-auto xl:flex-1-60 flex flex-col xl:pl-32 pb-20 relative">
                 <div className="project-image" style={({ backgroundImage: `url(${props.image})`})}>
                 </div>
-                <ProjectDecorationUp viewBox="0 0 221 209" className={`hidden md:block fill-current text-${colors.upperDecoration} absolute`} style={({ top: 0, right: 120 })} />
-                <ProjectDecorationDown viewBox="0 0 136 138" className={`hidden md:block absolute fill-current text-${colors.lowerDecoration}`} style={({ bottom: 90, right: 120 })}/>
+                <ProjectDecorationUp viewBox="0 0 221 209" className={`project-ud hidden md:block fill-current text-${colors.upperDecoration} absolute`} style={({ top: 0, right: 120 })} />
+                <ProjectDecorationDown viewBox="0 0 136 138" className={`project-dd hidden md:block absolute fill-current text-${colors.lowerDecoration}`} style={({ bottom: 90, right: 120 })}/>
             </div>
             <div className={`flex-auto xl:flex-1-40 flex flex-col text-${colors.text} md:tracking-widest`}>
                 <p className="flex-1-60 text-xl">
                     { props.description }
                 </p>
                 <h3 className="my-8 text-6xl md:text-7xl uppercase font-semibold" id={`${props.title}-title`}>{props.title}
-                <a href={props.link} rel="noreferrer" target="_blank" className="text-2xl align-top" title={`Visit ${props.title} in another tab`} aria-label={`Visit ${props.title} in another tab`}><span className="hidden">open link in a new tab</span><FontAwesomeIcon icon={["fas", "external-link-square-alt"]} className="mx-1"/></a></h3>
+                    <a href={props.link} rel="noreferrer" target="_blank" className="text-2xl align-top" title={`Visit ${props.title} in another tab`} aria-label={`Visit ${props.title} in another tab`}><span className="hidden">open link in a new tab</span><FontAwesomeIcon icon={["fas", "external-link-square-alt"]} className="mx-1"/></a></h3>
                 <ul className={`w-max flex flex-row flex-wrap justify-between border-t-4 border-${colors.text} py-5`}>
                     { props.tags.map((li, i) =>
                         <li key={i}
